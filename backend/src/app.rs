423562756aedeff14a3b068b8cf6cd9e8db4025c
allenyuchen <allenyuchen@hotmail.com>
2023-10-26 19:20:04 -0700

    add a probe api and fallback 404

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,6 +1,20 @@
 use crate::routes::users::router as users_router;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
 use axum::Router;
 
 pub fn api_router() -> Router {
-    Router::new().merge(users_router())
+    Router::new()
+        .route("/", get(probe))
+        .merge(users_router())
+        .fallback(not_found)
+}
+
+async fn probe() -> &'static str {
+    "OK"
+}
+
+async fn not_found() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Not Found")
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,6 +6,7 @@ async fn main() {
     let app = api_router();
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
